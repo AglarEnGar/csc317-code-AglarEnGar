e5bc7411edb111e6b801049d4ebdf82e74f3fee6
AglarEnGor <45472326+AglarEnGar@users.noreply.github.com>
2023-05-03 19:03:54 -0700

    Milestone Three Implementing Handlebars

--- application/routes/index.js
+++ application/routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CSC 317 App', name:"Luca Morgan" });
+  res.render('index', { title: 'CSC 317 App', name:"Luca Morgan", js:["index.js"]});
+});
+
+router.get("/login", function(req, res){
+  res.render('login',{ title: 'Login'});
+});
+
+router.get('/registration', function(req, res){
+  res.render('registration', { title: 'Registration', js:["validation.js"]});
+});
+
+router.get('/postvideo', function(req, res){
+  res.render('postvideo', { title: 'Post Video'});
+});
+
+router.get('/profile', function(req, res){
+  res.render('profile', { title: 'Profile'});
+});
+
+router.get('/viewpost/:id(\\d+)', function(req, res){
+  res.render('viewpost', { title: 'View Post'});
 });
 
 module.exports = router;
